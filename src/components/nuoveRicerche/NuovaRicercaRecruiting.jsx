00b8668806ffc61a6fe7389a2aa8838f14e9d242
carmicheal311 <andrearizza6@gmail.com>
2024-07-24 10:14:30 +0200

    chatGPT

--- src/components/nuoveRicerche/NuovaRicercaRecruiting.jsx
+++ src/components/nuoveRicerche/NuovaRicercaRecruiting.jsx
@@ -300,7 +300,6 @@
                         color: "white",
                         textTransform: "lowercase",
                         fontWeight: "bold",
-                        boxShadow: "9px 9px 9px rgba(0, 0, 0, 0.1)",
                         "&:hover": {
                             backgroundColor: '#116d0e',
                             color: "white",
@@ -319,7 +318,6 @@
                         color: "white",
                         textTransform: "lowercase",
                         fontWeight: "bold",
-                        boxShadow: "9px 9px 9px rgba(0, 0, 0, 0.1)",
                         "&:hover": {
                             backgroundColor: "#282828",
                             color: "white",
