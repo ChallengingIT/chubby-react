2bbe5afd0ec4ddce2a40923580474d8affed976a
msciuto <m.sciuto@inno-tek.it>
2025-02-04 21:38:34 +0100

    Url

--- src/components/CustomTableCell2.jsx
+++ src/components/CustomTableCell2.jsx
@@ -46,7 +46,7 @@ const CustomTableCell2 = ({ columns, rows, onRefresh, title }) => {
 
 const fetchStati = async () => {
     try {
-        const responseStato = await axios.get("http://localhost:8080/need/react/stato", { headers });
+        const responseStato = await axios.get("http://89.46.196.60:8443/need/react/stato", { headers });
 
         if (Array.isArray(responseStato.data)) {
             const filteredStati = responseStato.data
@@ -134,7 +134,7 @@ fetchStati();
     
         try {
             const responseUpdateStato = await axios.post(
-                `http://localhost:8080/need/react/salva/stato/${idNeed}?${params.toString()}`, 
+                `http://89.46.196.60:8443/need/react/salva/stato/${idNeed}?${params.toString()}`,
                 {}, 
                 { headers: headers }
             );
