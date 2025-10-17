c88f0763cb39ded1b0ac1eaedd68af8e6f44bd34
msciuto <m.sciuto@inno-tek.it>
2025-10-17 13:55:07 +0200

    url

--- src/components/card/KeypeopleCardFlip.jsx
+++ src/components/card/KeypeopleCardFlip.jsx
@@ -207,7 +207,7 @@ const KeypeopleCardFlip = ({ valori, statiOptions, onDelete, onRefresh, isFirstC
 
     const azioniKeypeople = async (id, event) => {
         try {
-            const responseAzioni = await axios.get(`http://localhost:8080/azioni/react/${id}`, { header: headers });
+            const responseAzioni = await axios.get(`http://89.46.196.60:8443/azioni/react/${id}`, { header: headers });
             if (Array.isArray(responseAzioni.data)) {
                 const azioni = responseAzioni.data.map((azione) => ({ ...azione }));
                 setAzioni(azioni);
@@ -231,7 +231,7 @@ const KeypeopleCardFlip = ({ valori, statiOptions, onDelete, onRefresh, isFirstC
             quantita: 10
         };
         try {
-            const responseNeed = await axios.get(`http://localhost:8080/need/react/keypeople/modificato/${id}`, { headers: headers, params: datiDaInviare });
+            const responseNeed = await axios.get(`http://89.46.196.60:8443/need/react/keypeople/modificato/${id}`, { headers: headers, params: datiDaInviare });
             if (Array.isArray(responseNeed.data)) {
                 const needDaAssociare = responseNeed.data.map((keypeople) => ({ ...keypeople }));
                 setNeedAssociati(needDaAssociare);
@@ -251,7 +251,7 @@ const KeypeopleCardFlip = ({ valori, statiOptions, onDelete, onRefresh, isFirstC
             quantita: 10
         };
         try {
-            const responseNeed = await axios.get(`http://localhost:8080/need/react/keypeople/modificato/${id}`, { headers: headers, params: datiDaInviare });
+            const responseNeed = await axios.get(`http://89.46.196.60:8443/need/react/keypeople/modificato/${id}`, { headers: headers, params: datiDaInviare });
             if (Array.isArray(responseNeed.data)) {
                 const needDaAssociare = responseNeed.data.map((keypeople) => ({ ...keypeople }));
                 setNeedAssociati(needDaAssociare);
@@ -272,7 +272,7 @@ const KeypeopleCardFlip = ({ valori, statiOptions, onDelete, onRefresh, isFirstC
 
     const azioniData = async (id, event) => {
         try {
-            const responseAzioni = await axios.get(`http://localhost:8080/azioni/react/tipologie`, { headers: headers });
+            const responseAzioni = await axios.get(`http://89.46.196.60:8443/azioni/react/tipologie`, { headers: headers });
             if (Array.isArray(responseAzioni.data)) {
                 setTipologieOptions(responseAzioni.data.map((tipologie) => ({ label: tipologie.descrizione, value: tipologie.id })));
             } else {
@@ -299,7 +299,7 @@ const KeypeopleCardFlip = ({ valori, statiOptions, onDelete, onRefresh, isFirstC
         const username = user?.username || null;
         const params = new URLSearchParams({ stato: idStato, username: username });
         try {
-            const responseUpdateStato = await axios.post(`http://localhost:8080/keypeople/react/salva/stato/${idKeypeople}?${params.toString()}`, {}, { headers: headers });
+            const responseUpdateStato = await axios.post(`http://89.46.196.60:8443/keypeople/react/salva/stato/${idKeypeople}?${params.toString()}`, {}, { headers: headers });
             setModalCambiaStato(false);
             onRefresh();
             handleOpenSnackbar(t('Stato aggiornato con successo!'), 'success');
@@ -423,7 +423,7 @@ const KeypeopleCardFlip = ({ valori, statiOptions, onDelete, onRefresh, isFirstC
 
         try {
             const response = await axios.post(
-                `http://localhost:8080/azioni/react/salva/${idKeyPeople}`,
+                `http://89.46.196.60:8443/azioni/react/salva/${idKeyPeople}`,
                 valoriDaInviare,
                 { headers }
             );
@@ -460,7 +460,7 @@ const KeypeopleCardFlip = ({ valori, statiOptions, onDelete, onRefresh, isFirstC
     const confirmDeleteAzione = async () => {
         try {
             const response = await axios.delete(
-                `http://localhost:8080/azioni/react/elimina/${azioneDaEliminare}`,
+                `http://89.46.196.60:8443/azioni/react/elimina/${azioneDaEliminare}`,
                 { header: headers }
             );
             if (response.data === "OK") {
