82b774b743fdaa40592e845e9cced45aec72ee73
marco.sciuto <marco.sciuto@cleep.it>
2025-12-11 14:14:23 +0100

    url

--- src/pages/Recruiting.jsx
+++ src/pages/Recruiting.jsx
@@ -142,7 +142,7 @@ const Recruiting = () => {
   useEffect(() => {
     const fetchSkills = async () => {
 
-      const responseAree = await axios.get("http://localhost:8080/staffing/react/areas", { headers });
+      const responseAree = await axios.get("http://80.211.138.142:8443/staffing/react/areas", { headers });
 
       let groupedSkills = [];
 
@@ -158,7 +158,7 @@ const Recruiting = () => {
           try {
             // Skill per area
             const responseSkillByArea = await axios.get(
-              `http://localhost:8080/staffing/react/skill/${area.id}`,
+              `http://80.211.138.142:8443/staffing/react/skill/${area.id}`,
               { headers }
             );
 
@@ -199,19 +199,19 @@ const Recruiting = () => {
 
     try {
       const response = await axios.get(
-        "http://localhost:8080/staffing/react/mod",
+        "http://80.211.138.142:8443/staffing/react/mod",
         { headers: headers, params: filtriDaInviare }
       );
       const responseTipologia = await axios.get(
-        "http://localhost:8080/aziende/react/tipologia",
+        "http://80.211.138.142:8443/aziende/react/tipologia",
         { headers }
       );
       const responseTipo = await axios.get(
-        "http://localhost:8080/staffing/react/tipo",
+        "http://80.211.138.142:8443/staffing/react/tipo",
         { headers }
       );
       const responseStato = await axios.get(
-        "http://localhost:8080/staffing/react/stato/candidato",
+        "http://80.211.138.142:8443/staffing/react/stato/candidato",
         { headers }
       );
 
@@ -337,8 +337,8 @@ const Recruiting = () => {
     );
 
     const url = filtriAttivi
-      ? "http://localhost:8080/staffing/react/filtri/ricerca"
-      : "http://localhost:8080/staffing/react/mod";
+      ? "http://80.211.138.142:8443/staffing/react/filtri/ricerca"
+      : "http://80.211.138.142:8443/staffing/react/mod";
 
     const filtriDaInviare = {
       nome: currentFilters.nome || null,
@@ -406,7 +406,7 @@ const Recruiting = () => {
   const handleDelete = async () => {
     try {
       const responseDelete = await axios.delete(
-        `http://localhost:8080/staffing/elimina/${deleteId}`,
+        `http://80.211.138.142:8443/staffing/elimina/${deleteId}`,
         { headers: headers }
       );
       setOpenDialog(false);
@@ -453,19 +453,19 @@ const Recruiting = () => {
 
     try {
       const response = await axios.get(
-        "http://localhost:8080/staffing/react/mod/ricerca",
+        "http://80.211.138.142:8443/staffing/react/mod/ricerca",
         { headers: headers, params: filtriDaInviare }
       );
       const responseTipologia = await axios.get(
-        "http://localhost:8080/aziende/react/tipologia",
+        "http://80.211.138.142:8443/aziende/react/tipologia",
         { headers: headers }
       );
       const responseTipo = await axios.get(
-        "http://localhost:8080/staffing/react/tipo",
+        "http://80.211.138.142:8443/staffing/react/tipo",
         { headers: headers }
       );
       const responseStato = await axios.get(
-        "http://localhost:8080/staffing/react/stato/candidato",
+        "http://80.211.138.142:8443/staffing/react/stato/candidato",
         { headers: headers }
       );
 
@@ -568,7 +568,7 @@ const Recruiting = () => {
 
 
   const handleDownloadCV = async (idFile, fileDescrizione) => {
-    const url = `http://localhost:8080/files/react/download/file/${idFile}`;
+    const url = `http://80.211.138.142:8443/files/react/download/file/${idFile}`;
     try {
       const responseDownloadCV = await axios({
         method: "GET",
@@ -648,7 +648,7 @@ const Recruiting = () => {
   const handleDownloadCF = async (idCandidato, nomeCandidato, cognomeCandidato, tipo) => {
     try {
       setLoadingCF(true);
-      const downloadUrl = `http://localhost:8080/files/download/cf/${idCandidato}`;
+      const downloadUrl = `http://80.211.138.142:8443/files/download/cf/${idCandidato}`;
       const params = new URLSearchParams({ tipo });
 
       const res = await axios({
@@ -730,8 +730,8 @@ const Recruiting = () => {
     const params = new URLSearchParams({ stato: idStato });
     try {
       const responseUpdateStato = await axios.post
-        // (`http://localhost:8080/keypeople/react/salva/stato/${idKeypeople}?${params.toString()}`, {}, { headers: headers });
-        (`http://localhost:8080/staffing/react/salva/stato/${idCandidato}?${params.toString()}`, {}, { headers: headers });
+        // (`http://80.211.138.142:8443/keypeople/react/salva/stato/${idKeypeople}?${params.toString()}`, {}, { headers: headers });
+        (`http://80.211.138.142:8443/staffing/react/salva/stato/${idCandidato}?${params.toString()}`, {}, { headers: headers });
       setModalCambiaStato(false);
       fetchData();
       handleOpenSnackbar(t('Stato aggiornato con successo!'), 'success');
